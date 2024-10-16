09708b8f2cafa44e096a170867f4baa547577c67
OTFT <sonic07@comcast.net>
2024-10-16 10:26:12 -0400

    Made different webpace for meeting characters

--- reusediv.js
+++ reusediv.js
@@ -1,1 +1,5 @@
-$('#weblinksDiv').load('reuasblewebpagelinks.html');
+const template = document.getElementById('weblinksDiv');
+
+template.innerHTML = `<div style="background-color: black;color: white;display: flex;justify-content: center;align-items: center;flex-direction: column;"><div style="text-size-adjust: revert;display: flex;justify-content: center;align-items: center;"><a href="https://otft.github.io/otft_tvg/">Homepage</a><text> · </text><a href="meetchars">Meet the Characters</a><text> · </text><a href="FAQ">FAQ</a><text> · </text><a href="Contact">Contact Me!</a></div>`;
+
+document.getElementById('headerthingie').appendChild(template.content);
