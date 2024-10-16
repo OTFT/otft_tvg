64ab4ddb10e9651e2cf96172a3735f24ef3bd8c1
OTFT <sonic07@comcast.net>
2024-10-16 15:17:15 -0400

    Added About Page

--- reusablesubdiv.js
+++ reusablesubdiv.js
@@ -1,5 +1,5 @@
 const template1 = document.getElementById('weblinksDiv01');
 
-template1.innerHTML = `<div style="height:60px;text-size-adjust: revert;display: flex;justify-content: center;align-items: center;"><a href="https://otft.github.io/otft_tvg/">Homepage</a><text> · </text><a href="meetchars">Meet the Characters</a><text> · </text><a href="FAQ">FAQ</a><text> · </text><a href="Contact">Contact Me!</a></div>`;
+template1.innerHTML = `<div style="height:60px;text-size-adjust: revert;display: flex;justify-content: center;align-items: center;"><a href="https://otft.github.io/otft_tvg/">Homepage</a><text> · </text><a href="https://otft.github.io/otft_tvg/meetchars">Meet the Characters</a><text> · </text><a href="FAQ">FAQ</a><text> · </text><a href="https://otft.github.io/otft_tvg/about">About</a></div>`;
 
 document.getElementById('footerthingie').appendChild(template1.content);
--- reusedivfix.js
+++ reusedivfix.js
@@ -1,5 +1,5 @@
 const template = document.getElementById('weblinksDiv');
 
-template.innerHTML = `<div style="background-color: black;color: white;display: flex;justify-content: center;align-items: center;flex-direction: column;"><div style="text-size-adjust: revert;display: flex;justify-content: center;align-items: center;"><a href="https://otft.github.io/otft_tvg/">Homepage</a><text> · </text><a href="meetchars">Meet the Characters</a><text> · </text><a href="FAQ">FAQ</a><text> · </text><a href="Contact">Contact Me!</a></div>`;
+template.innerHTML = `<div style="background-color: black;color: white;display: flex;justify-content: center;align-items: center;flex-direction: column;"><div style="text-size-adjust: revert;display: flex;justify-content: center;align-items: center;"><a href="https://otft.github.io/otft_tvg/">Homepage</a><text> · </text><a href="https://otft.github.io/otft_tvg/meetchars">Meet the Characters</a><text> · </text><a href="FAQ">FAQ</a><text> · </text><a href="https://otft.github.io/otft_tvg/about">About</a></div>`;
 
 document.getElementById('headerthingie').appendChild(template.content);
